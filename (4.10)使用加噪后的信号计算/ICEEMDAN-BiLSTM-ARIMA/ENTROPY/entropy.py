18d197b44e6b8f90ed18b34e2f5dfed19f6aba56
435191950@qq.com <435191950@qq.com>
2024-05-15 08:59:15 +0800

    在模拟信号中加入噪声

---
+++ (4.10)使用加噪后的信号计算/ICEEMDAN-BiLSTM-ARIMA/ENTROPY/entropy.py
@@ -0,0 +1,117 @@
+__all__ = ["ENTROPY"]
+
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+
+class SampEn(object):
+    """
+    SampEn
+    Version 0.1.0
+    """
+
+    def __init__(self):
+        pass
+
+    def condprob(self, L, m, r):
+        """
+        Calculates the conditional probability A/B of a time series
+
+        Input:
+            L: Time series
+            m: Template length
+            r: Tolerance level
+
+        Output:
+            Conditional probability
+        """
+        N = len(L)
+        B = 0.0
+        A = 0.0
+
+        # Split time series and save all templates of length m
+        xmi = np.array([L[i:i + m] for i in range(N - m)])
+        xmj = np.array([L[i:i + m] for i in range(N - m + 1)])
+
+        # Save all matches minus the self-match, compute B
+        B = np.sum([np.sum(np.abs(xmii - xmj).max(axis=1) <= r) - 1 for xmii in xmi])
+
+        # Similar for computing A
+        m += 1
+        xm = np.array([L[i:i + m] for i in range(N - m + 1)])
+
+        A = np.sum([np.sum(np.abs(xmi - xm).max(axis=1) <= r) - 1 for xmi in xm])
+
+        # Return conditional probability
+        return A / B
+
+    def sampen(self, L, m, r):
+        """
+        Calculates sample entropy (SE) of a time series
+
+        Input:
+            L: Time series
+            m: Template length
+            r: Tolerance level
+
+        Output:
+            SampEn
+        """
+        N = len(L)
+        B = 0.0
+        A = 0.0
+
+        # Split time series and save all templates of length m
+        xmi = np.array([L[i:i + m] for i in range(N - m)])
+        xmj = np.array([L[i:i + m] for i in range(N - m + 1)])
+
+        # Save all matches minus the self-match, compute B
+        B = np.sum([np.sum(np.abs(xmii - xmj).max(axis=1) <= r) - 1 for xmii in xmi])
+
+        # Similar for computing A
+        m += 1
+        xm = np.array([L[i:i + m] for i in range(N - m + 1)])
+
+        A = np.sum([np.sum(np.abs(xmi - xm).max(axis=1) <= r) - 1 for xmi in xm])
+
+        # Return SampEn
+        return -np.log(A / B)
+
+    def qse(self, L, m, r):
+        """
+        Calculates quadratic sample entropy (QSE) of a time series
+
+        Input:
+            L: Time series
+            m: Template length
+            r: Tolerance level
+
+        Output:
+            QSE
+        """
+        N = len(L)
+        B = 0.0
+        A = 0.0
+
+        # Split time series and save all templates of length m
+        xmi = np.array([L[i:i + m] for i in range(N - m)])
+        xmj = np.array([L[i:i + m] for i in range(N - m + 1)])
+
+        # Save all matches minus the self-match, compute B
+        B = np.sum([np.sum(np.abs(xmii - xmj).max(axis=1) <= r) - 1 for xmii in xmi])
+
+        # Similar for computing A
+        m += 1
+        xm = np.array([L[i:i + m] for i in range(N - m + 1)])
+
+        A = np.sum([np.sum(np.abs(xmi - xm).max(axis=1) <= r) - 1 for xmi in xm])
+
+        # return QSE
+        return -np.log(A / B) + np.log(2 * r)
+
+
+_inst = SampEn()
+condprob = _inst.condprob
+sampen = _inst.sampen
+qse = _inst.qse
