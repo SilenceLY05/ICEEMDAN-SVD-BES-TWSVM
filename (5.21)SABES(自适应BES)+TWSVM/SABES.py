8932c7df5b4ba8bfd259edb76a836e736e074675
435191950@qq.com <435191950@qq.com>
2024-06-29 11:03:37 +0800

    SABES

---
+++ (5.21)SABES(自适应BES)+TWSVM/SABES.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# ------------------------------------------------------------------------------------------------------%
+# Created by "Thieu Nguyen" at 14:52, 17/03/2020                                                        %
+#                                                                                                       %
+#       Email:      nguyenthieu2102@gmail.com                                                           %
+#       Homepage:   https://www.researchgate.net/profile/Thieu_Nguyen6                                  %
+#       Github:     https://github.com/thieu1995                                                        %
+#-------------------------------------------------------------------------------------------------------%
+
+import numpy as np
+from mealpy.optimizer import Optimizer
+
+
+class BaseSABES(Optimizer):
+    """
+    Original version of: Bald Eagle Search (BES)
+        (Novel meta-heuristic bald eagle search optimisation algorithm)
+    Link:
+        DOI: https://doi.org/10.1007/s10462-019-09732-5
+    """
+
+    def __init__(self, problem, epoch=10000, pop_size=100, a=10, R=1.5, alpha=2, c1=2, c2=2, **kwargs):
+        """
+        Args:
+            problem ():
+            epoch (int): maximum number of iterations, default = 10000
+            pop_size (int): number of population size, default = 100
+            a (int): default: 10, determining the corner between point search in the central point, in [5, 10]
+            R (float): default: 1.5, determining the number of search cycles, in [0.5, 2]
+            alpha (float): default: 2, parameter for controlling the changes in position, in [1.5, 2]
+            c1 (float): default: 2, in [1, 2]
+            c2 (float): c1 and c2 increase the movement intensity of bald eagles towards the best and centre points
+            **kwargs ():
+        """
+        super().__init__(problem, kwargs)
+        self.epoch = epoch
+        self.pop_size = pop_size
+        self.a = a
+        self.R = R
+        self.alpha = alpha
+        self.c1 = c1
+        self.c2 = c2
+        self.nfe_per_epoch = 3 * pop_size
+        self.sort_flag = False
+
+        # Initialize population using equation (7)
+        self.pop = [self.create_solution() for _ in range(self.pop_size)]
+
+    def create_solution(self):
+        solution = np.random.uniform(self.problem.lb, self.problem.ub)
+        return [solution, None]
+
+    def dynamic_opposite(self, pos, lb, ub):
+        S_O = lb + ub - pos
+        return pos + np.random.uniform() * (np.random.uniform() * S_O - pos)
+
+    def _create_x_y_x1_y1_(self, epoch):
+        t_max = self.epoch
+        phi = (self.a * np.pi * np.random.uniform(0, 1, self.pop_size))
+        r = phi + self.R * np.random.uniform(0, 1, self.pop_size)
+        xr, yr = r * np.sin(phi), r * np.cos(phi)
+
+        r1 = phi1 = self.a * np.pi * np.random.uniform(0, 1, self.pop_size)
+        xr1, yr1 = r1 * np.sinh(phi1), r1 * np.cosh(phi1)
+
+        x_list = xr / np.max(np.abs(xr))
+        y_list = yr / np.max(np.abs(yr))
+        x1_list = xr1 / np.max(np.abs(xr1))
+        y1_list = yr1 / np.max(np.abs(yr1))
+
+        a_min, a_max = 5, 10
+        R_min, R_max = 0.5, 2
+        alpha_min, alpha_max = 1.5, 2
+        n = 1.2
+
+        self.a = ((t_max - epoch) / t_max) ** n * (a_max - a_min) + a_min
+        self.R = ((t_max - epoch) / t_max) ** n * (R_max - R_min) + R_min
+        self.alpha = ((t_max - epoch) / t_max) ** n * (alpha_max - alpha_min) + alpha_min
+
+        return x_list, y_list, x1_list, y1_list
+
+    def evolve(self, epoch):
+        t_max = self.epoch
+        x_list, y_list, x1_list, y1_list = self._create_x_y_x1_y1_(epoch)
+
+        # Initialization with Dynamic-Opposite Learning
+        for individual in self.pop:
+            individual[self.ID_POS] = self.dynamic_opposite(individual[self.ID_POS], self.problem.lb, self.problem.ub)
+
+        # Calculate fitness of initial population
+        self.pop = self.update_fitness_population(self.pop)
+        self.pop = sorted(self.pop, key=lambda x: x[self.ID_FIT])
+        self.pop = self.pop[:self.pop_size]
+
+        # Select Phase
+        pos_list = np.array([individual[self.ID_POS] for individual in self.pop])
+        pos_mean = np.mean(pos_list, axis=0)
+
+        pop_new = []
+        for idx in range(self.pop_size):
+            pos_new = self.g_best[self.ID_POS] + self.alpha * np.random.uniform() * (pos_mean - self.pop[idx][self.ID_POS])
+            pos_new = self.amend_position_faster(pos_new)
+            pop_new.append([pos_new, None])
+        pop_new = self.update_fitness_population(pop_new)
+        pop_new = self.greedy_selection_population(self.pop, pop_new)
+
+        # Update fitness values and best search agent
+        self.pop = pop_new
+        self.g_best = min(self.pop, key=lambda x: x[self.ID_FIT])
+
+        # Search Phase
+        pos_list = np.array([individual[self.ID_POS] for individual in self.pop])
+        pos_mean = np.mean(pos_list, axis=0)
+
+        pop_child = []
+        for idx in range(self.pop_size):
+            idx_rand = np.random.choice(list(set(range(self.pop_size)) - {idx}))
+            pos_new = self.pop[idx][self.ID_POS] + y_list[idx] * (self.pop[idx][self.ID_POS] - self.pop[idx_rand][self.ID_POS]) + \
+                      x_list[idx] * (self.pop[idx][self.ID_POS] - pos_mean)
+            pos_new = self.dynamic_opposite(pos_new, self.problem.lb, self.problem.ub)
+            pos_new = self.amend_position_faster(pos_new)
+            pop_child.append([pos_new, None])
+        pop_child = self.update_fitness_population(pop_child)
+        pop_child = self.greedy_selection_population(self.pop, pop_child)
+
+        # Update best search agent
+        self.pop = pop_child
+        self.g_best = min(self.pop, key=lambda x: x[self.ID_FIT])
+
+        # Swoop Phase
+        pos_list = np.array([individual[self.ID_POS] for individual in self.pop])
+        pos_mean = np.mean(pos_list, axis=0)
+
+        a_min, a_max = 5, 10
+        c1_min, c1_max = 0.5, 2
+        c2_min, c2_max = 0.5, 2
+
+        self.a = ((t_max - epoch) / t_max) * (a_max - a_min) + a_min
+        self.c1 = ((t_max - epoch) / t_max) * (c1_max - c1_min) + c1_min
+        self.c2 = ((t_max - epoch) / t_max) * (c2_max - c2_min) + c2_min
+
+        pop_new = []
+        for idx in range(self.pop_size):
+            pos_new = np.random.uniform() * self.g_best[self.ID_POS] + x1_list[idx] * (self.pop[idx][self.ID_POS] - self.c1 * pos_mean) + \
+                      y1_list[idx] * (self.pop[idx][self.ID_POS] - self.c2 * self.g_best[self.ID_POS])
+            pos_new = self.amend_position_faster(pos_new)
+            pop_new.append([pos_new, None])
+        pop_new = self.update_fitness_population(pop_new)
+        self.pop = self.greedy_selection_population(self.pop, pop_new)
+
+        # Update fitness values and best search agent
+        self.pop = pop_new
+        self.g_best = min(self.pop, key=lambda x: x[self.ID_FIT])
